4e5ea4f4e99539c489ff798ec2ef1e2e34256ea0
GanisAD <ganis.ahmad.com@gmail.com>
2025-11-22 17:19:47 +0700

    Card view and also Card holder
    Making card view and card holder, not included card system yet

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text durability;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private SpriteRenderer card_image;
+    [SerializeField] private GameObject wrapper;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
