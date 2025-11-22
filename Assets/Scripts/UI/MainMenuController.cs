d3a9450d2cb97ad3ffb59c61098436a5d5bf5777
Nizar-MNNA <mohnizar2005@gmail.com>
2025-11-22 15:44:13 +0700

    style(main-menu): create simple layout and button function for main menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -8,12 +8,12 @@ public class MainMenu : MonoBehaviour
     public void ExitButton()
     {
         Debug.Log("Exit button pressed. Application quitting...");
-        // Application.Quit();
+        Application.Quit();
     }
 
     public void StartGameButton()
     {
         Debug.Log("Start Game button pressed. Loading game scene...");
-        // SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("02_MapScene");
     }
 }
