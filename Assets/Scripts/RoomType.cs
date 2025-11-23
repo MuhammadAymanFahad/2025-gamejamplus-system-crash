76669cdd1bdf85c678d7d10dd99b8b87b29a73ab
Udiin <130551799+g3evorz@users.noreply.github.com>
2025-11-23 16:13:22 +0700

    World movement (#11)
    * .
    
    * Movement + Rooms Logic (unfinished)
    
    * Movement and Room Randomizer
    
    * tes
    
    * Wall Collision
    
    * Level Management

--- Assets/Scripts/RoomType.cs
+++ Assets/Scripts/RoomType.cs
@@ -4,7 +4,6 @@ public enum RoomType
 {
     Empty,    // Ruangan kosong, jarang digunakan di level 1
     Combat,   // Pertarungan/Fight Scene
-    Reward,   // Hadiah/Reward Scene (e.g., Chest, Healing)
     Boss,     // Ruangan Boss
     Entrance, // Ruangan Awal Player
     Exit      // Pintu Keluar (Tidak diperlukan jika hanya 1 Boss room)
