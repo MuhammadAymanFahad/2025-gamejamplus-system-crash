edff8c651274aeee713fbef3457d83babb035b2e
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2026-01-08 23:40:48 +0700

    feat(level-system): add weapon inventory logic on script

--- Assets/Scripts/NewSystem/PlayerInventory.cs
+++ Assets/Scripts/NewSystem/PlayerInventory.cs
@@ -21,4 +21,40 @@ public class PlayerInventory : MonoBehaviour
         potionStack.Push(potion);
         Debug.Log("Added potion, grade : " + potion.grade + ", total potion : " + potionStack.Count);
     }
+
+    public CardSO UsePotion()
+    {
+        if(potionStack.Count == 0 )
+        {
+            return null;
+        }
+        var potion = potionStack.Pop();
+        Debug.Log("Used potion, grade : " + potion.grade + ", remaining potion : " + potionStack.Count);
+        return potion;
+    }
+
+    public void PickupWeapon(CardSO weaponCard)
+    {
+        var weapon = new WeaponInstance(weaponCard.grade, "pickup");
+        weaponStack.Push(weapon);
+        Debug.Log("Picked up weapon, grade : " + weapon.grade + ", total weapons : " + weaponStack.Count);
+    }
+
+    public void AddWeaponFromEnemy(int EnemyGrade)
+    {
+        var weapon = new WeaponInstance(EnemyGrade, "enemy");
+        weaponStack.Push(weapon);
+        Debug.Log("Added weapon from enemy, grade : " + weapon.grade + ", total weapons : " + weaponStack.Count);
+    }
+
+    public void DiscardWeapon()
+    {
+        weaponStack.Clear();
+        Debug.Log("All weapons discarded, total weapons : " + weaponStack.Count);
+    }
+
+    public WeaponInstance GetCurrectWeapon()
+    {
+        return weaponStack.Count > 0 ? weaponStack.Peek() : null;
+    }
 }
